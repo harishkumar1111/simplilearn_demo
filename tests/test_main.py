a9b6d728870bce250ade854dd038ceb8cc6e23a1
Harish Kumar <46653162+harishkumar1111@users.noreply.github.com>
2025-04-02 23:29:14 +0530

    Create test_main.py

---
+++ tests/test_main.py
@@ -0,0 +1,14 @@
+import pytest
+from simplilearn_demo.main import greet
+
+def test_greet_with_name():
+    assert greet("Harish") == "Hello, Harish!"
+
+def test_greet_with_empty_string():
+    assert greet("") == "Hello, !"
+
+def test_greet_with_special_characters():
+    assert greet("@123") == "Hello, @123!"
+
+def test_greet_with_numbers():
+    assert greet("1234") == "Hello, 1234!"
